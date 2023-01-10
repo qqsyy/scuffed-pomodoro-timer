32f1c2507117ade9116fde300f454c8af01e4209
qqsy <beetulplayz@gmail.com>
2023-01-10 13:47:03 +1000

    added lambdas

--- src/pomodoroTimer.java
+++ src/pomodoroTimer.java
@@ -1,11 +1,8 @@
 import javax.sound.sampled.Clip;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 import java.io.File;
-import java.util.Objects;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 
@@ -46,79 +43,64 @@ public class pomodoroTimer extends JFrame{
         final Timer[] t = new Timer[1];
         final int[] k = {1499};
 
-        t[0] = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // convert into minutes and seconds
-                int m = k[0] % 3600 / 60;
-                int k2 = k[0] % 3600 % 60;
-                String mStr = String.valueOf(m);
-                String k2Str = String.valueOf(k2);
-                k[0]--;
+        t[0] = new Timer(1000, e -> {
+            // convert into minutes and seconds
+            int m = k[0] % 3600 / 60;
+            int k2 = k[0] % 3600 % 60;
+            String mStr = String.valueOf(m);
+            String k2Str = String.valueOf(k2);
+            k[0]--;
 
-                if(m < 10 && k2 < 10){ // Concatenate 0 when one of the values becomes a singular digit
-                    pTimer.setText("0" + mStr + ":" + "0" + k2Str);
-                } else if (m < 10){
-                    pTimer.setText("0" + mStr + ":" + k2Str);
-                } else if (k2 < 10){
-                    pTimer.setText(mStr + ":" + "0" + k2Str);
-                } else {
-                    pTimer.setText(mStr + ":" + k2Str);
-                }
-                if(k[0] == -1){
-                    t[0].stop();
-                }
+            if(m < 10 && k2 < 10){ // Concatenate 0 when one of the values becomes a singular digit
+                pTimer.setText("0" + mStr + ":" + "0" + k2Str);
+            } else if (m < 10){
+                pTimer.setText("0" + mStr + ":" + k2Str);
+            } else if (k2 < 10){
+                pTimer.setText(mStr + ":" + "0" + k2Str);
+            } else {
+                pTimer.setText(mStr + ":" + k2Str);
             }
-        });
-
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
-                t[0].start();
-            }
-        });
-        stopButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
+            if(k[0] == -1){
                 t[0].stop();
             }
         });
 
-        pomodoroButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                k[0] = 1499;
-                pTimer.setText("25:00");
-                playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
-            }
+        startButton.addActionListener(e -> {
+            playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
+            t[0].start();
         });
-        shortBreakButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                k[0] = 299;
-                pTimer.setText("05:00");
-                playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
-            }
+        stopButton.addActionListener(e -> {
+            playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
+            t[0].stop();
         });
-        longBreakButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                k[0] = 899;
-                pTimer.setText("15:00");
-                playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
-            }
+        pomodoroButton.addActionListener(e -> {
+            k[0] = 1499;
+            pTimer.setText("25:00");
+            playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
+        });
+        shortBreakButton.addActionListener(e -> {
+            k[0] = 299;
+            pTimer.setText("05:00");
+            playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
+        });
+        longBreakButton.addActionListener(e -> {
+            k[0] = 899;
+            pTimer.setText("15:00");
+            playMusic("C:\\Users\\Bondo\\OneDrive\\Desktop\\Intellij Projects\\pomodoroTimer\\src\\Audio\\Button.wav");
         });
     }
 
-    public static void main(String[] args){
-        pomodoroTimer frame = new pomodoroTimer();
+    public static void frameSettings(pomodoroTimer frame) {
         frame.setContentPane(new pomodoroTimer().Main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setName("Pomodoro Timer");
         frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        pomodoroTimer frame = new pomodoroTimer();
+        frameSettings(frame);
     }
 }
